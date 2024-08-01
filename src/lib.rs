19d2d71d2d3b22b945837ccd240e9d7e6947d5c8
72mins <72min@proton.me>
2024-08-01 23:30:47 +0200

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use actix_files::Files;
+use actix_web::{dev::Server, middleware, web, App, HttpServer};
+use std::net::TcpListener;
+use tera::Tera;
+
+pub mod handlers;
+
+#[macro_use]
+extern crate lazy_static;
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+
+        tera.autoescape_on(vec![".html", ".sql"]);
+        tera
+    };
+}
+
+pub fn start_server(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server: Server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(TEMPLATES.clone()))
+            .wrap(middleware::Logger::default())
+            .service(Files::new("/static", "static/").use_last_modified(true))
+            .service(handlers::index)
+            .default_service(web::route().to(handlers::not_found))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
