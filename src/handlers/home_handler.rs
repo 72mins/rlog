e7c7e6281e9bdab507f4e5946322beec9c65aa75
72mins <72min@proton.me>
2024-08-03 21:14:00 +0200

    init

--- src/handlers/home_handler.rs
+++ src/handlers/home_handler.rs
@@ -14,28 +14,28 @@ pub struct Frontmatter {
 }
 #[get("/")]
 pub async fn index(templates: web::Data<tera::Tera>) -> impl Responder {
-   let mut context = tera::Context::new();
-   
-   let frontmatters = vec![Frontmatter{
-	tags: vec!["Rusty".to_string(), "Test".to_string()],
-	title: "Test posts".to_string(),
-	file_name: "test_posts.md".to_string(),
-	description: "Just testing out the system".to_string(),
-	posted: "2022-08-09".to_string(),
-	author: "MBvisti".to_string(),
-	estimated_reading_time: 12,
-	order: 1,
-   }];
-   
-   context.insert("posts", &frontmatters);
-   
-   match templates.render("home.html", &context) {
-	Ok(s) => HttpResponse::Ok().content_type("text/html").body(s),
-	Err(e) => {
-		println!("{:?}", e);
-		HttpResponse::InternalServerError()
-			.content_type("text/html")
-			.body("<p>Something went wrong!</p>")
-	}
-   }
+    let mut context = tera::Context::new();
+
+    let frontmatters = vec![Frontmatter {
+        tags: vec!["Rusty".to_string(), "Test".to_string()],
+        title: "Test posts".to_string(),
+        file_name: "test_posts.md".to_string(),
+        description: "Just testing out the system".to_string(),
+        posted: "2022-08-09".to_string(),
+        author: "MBvisti".to_string(),
+        estimated_reading_time: 12,
+        order: 1,
+    }];
+
+    context.insert("posts", &frontmatters);
+
+    match templates.render("home.html", &context) {
+        Ok(s) => HttpResponse::Ok().content_type("text/html").body(s),
+        Err(e) => {
+            println!("{:?}", e);
+            HttpResponse::InternalServerError()
+                .content_type("text/html")
+                .body("<p>Something went wrong!</p>")
+        }
+    }
 }
